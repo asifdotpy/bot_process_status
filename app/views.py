0106c69f0e967bb4deda90247d26ace980010cc0
root <root@server1.novogad.com>
2023-07-18 08:32:19 +0000

    user desired json output added. every bot has different json data

--- app/views.py
+++ app/views.py
@@ -1,30 +1,15 @@
-# Import the necessary modules
 import os
 import subprocess
 import logging
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-# Define a function to check the status of a process by its name
-def check_process_status(process_name):
-    # Use ps command to get the process information
-    ps = subprocess.Popen(['ps', '-aux'], stdout=subprocess.PIPE).communicate()[0]
-    # Decode the output and split it by lines
-    processes = ps.decode().split('\n')
-    # Loop through the processes and look for the matching name
-    for process in processes:
-        if process_name in process:
-            # If found, return the process information as a dictionary
-            fields = process.split()
-            return {
-                'pid': fields[1],
-                'user': fields[0],
-                'cpu': fields[2],
-                'mem': fields[3],
-                'command': fields[-1]
-            }
-    # If not found, return None
-    return None
+# Set up a logger with a file handler and a formatter
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('app.log')
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(file_handler)
 
 # Define a list of processes to monitor
 processes = [
@@ -55,6 +40,27 @@ processes = [
     }
 ]
 
+# Define a function to check the status of a process by its name
+def check_process_status(process_name):
+    # Use ps command to get the process information
+    ps = subprocess.Popen(['ps', '-aux'], stdout=subprocess.PIPE).communicate()[0]
+    # Decode the output and split it by lines
+    processes = ps.decode().split('\n')
+    # Loop through the processes and look for the matching name
+    for process in processes:
+        if process_name in process:
+            # If found, return the process information as a dictionary
+            fields = process.split()
+            return {
+                'pid': fields[1],
+                'user': fields[0],
+                'cpu': fields[2],
+                'mem': fields[3],
+                'command': fields[-1]
+            }
+    # If not found, return None
+    return None
+
 # Define a function to read the last line of a log file
 def read_last_log_line(log_file):
     # Open the log file in read mode
@@ -72,41 +78,58 @@ def read_last_log_line(log_file):
                 break
         # Return the last line of the file
         return f.readline()
-    
+
 # Define an api view function to handle the request for the status of the processes
 @api_view(['GET'])
 def get_process_status(request):
-    # Create an empty list to store the status of each process
+    # Indent the code inside the function by four spaces
     status_list = []
     # Loop through the processes to monitor
     for process in processes:
+        # Create an empty dictionary to store the status of the current process
+        status_dict = {}
         # Check the status of the process by its name
         status = check_process_status(process['name'])
         # If the status is not None, it means the process is running
         if status:
-            # Add a dictionary with the status and a message to the list
-            status_list.append({
-                'status': status,
-                'message': f"{process['name']} is running"
-            })
-        # If the status is None, it means the process is not running or stopped with error
-        else:
             # Check if the process has a log file
             if process['log']:
                 # Read the last line of the log file
                 last_log_line = read_last_log_line(process['log'])
-                # Add a dictionary with the path and a message with the error to the list
-                status_list.append({
-                    'path': process['path'],
-                    'message': f"{process['name']} stopped with error: {last_log_line}"
-                })
-            # If the process does not have a log file, just add a dictionary with the path and a message to the list
+                # Check if the last line contains an error message
+                if 'error' in last_log_line.lower():
+                    # Log an error message with the process name and the log line
+                    logger.error(f"{process['name']} is running with error: {last_log_line}")
+                    # Add an entry to the status dictionary with the process name, error log and status code 2 (run with error)
+                    status_dict[process['name']] = 'running with error'
+                    status_dict['error_log'] = last_log_line.strip()
+                    status_dict['status_code'] = 2
+                else:
+                    # Log an info message with the process name and status code 1 (running)
+                    logger.info(f"{process['name']} is running")
+                    # Add an entry to the status dictionary with the process name, error log and status code 1 (running)
+                    status_dict[process['name']] = 'running'
+                    status_dict['error_log'] = last_log_line.strip()
+                    status_dict['status_code'] = 1
             else:
-                status_list.append({
-                    'path': process['path'],
-                    'message': f"{process['name']} stopped or not running"
-                })
+                # Log an info message with the process name and status code 1 (running)
+                logger.info(f"{process['name']} is running")
+                # Add an entry to the status dictionary with the process name, no log and status code 1 (running)
+                status_dict[process['name']] = 'running'
+                status_dict['error_log'] = 'No log'
+                status_dict['status_code'] = 1
+            
+        # If the status is None, it means the process is not running or stopped with error
+        else:
+            # Log a warning message with the process name and path
+            logger.warning(f"{process['name']} stopped or not running at {process['path']}")
+            # Add an entry to the status dictionary with the process name, path and status code 3 (down)
+            status_dict[process['name']] = 'down'
+            status_dict['path'] = process['path']
+            status_dict['status_code'] = 3
+        
+        # Append the status dictionary to the status list
+        status_list.append(status_dict)
     
     # Return a response with the status list as data and a 200 OK status code 
     return Response(data=status_list, status=200)
-
